0c3053bfce23ab518ba92dde44b21304cb030ba7
harika91 <harika91@users.noreply.github.com>
2019-01-16 10:48:24 -0600

    Update EnrichmentWebService.java
    Remove aspect from request

--- src/main/java/edu/mcw/rgd/web/EnrichmentWebService.java
+++ src/main/java/edu/mcw/rgd/web/EnrichmentWebService.java
@@ -5,6 +5,7 @@ import edu.mcw.rgd.dao.impl.GeneDAO;
 import edu.mcw.rgd.dao.impl.GeneEnrichmentDAO;
 import edu.mcw.rgd.dao.impl.OntologyXDAO;
 import edu.mcw.rgd.datamodel.Gene;
+import edu.mcw.rgd.datamodel.ontologyx.Aspect;
 import edu.mcw.rgd.process.enrichment.geneOntology.GeneOntologyEnrichmentProcess;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -30,44 +31,56 @@ public class EnrichmentWebService {
     OntologyXDAO oDao = new OntologyXDAO();
     @RequestMapping(value = "/enrichment/chart/{speciesTypeKey}/{genes}/{aspect}", method = RequestMethod.GET)
     @ApiOperation(value = "Return a chart of ontology terms annotated to the genes.Genes are rgdids separated by comma.Species type is an integer value.Aspect is the Ontology group")
-    public ArrayList getChart(@ApiParam(value = "Species Type Key - 3=rat ", required = true) @PathVariable(value = "speciesTypeKey") int speciesTypeKey,
-                            @ApiParam(value = "List of RGDids", required = true) @PathVariable(value = "genes") String genes,
-                            @ApiParam(value = "List of ontology groups", required = true) @PathVariable(value = "aspect") String aspect) throws Exception {
+    public HashMap getChart(@ApiParam(value = "Species Type Key - 3=rat ", required = true) @PathVariable(value = "speciesTypeKey") int speciesTypeKey,
+                            @ApiParam(value = "List of RGDids", required = true) @PathVariable(value = "genes") String genes)
+                             throws Exception {
 
         String[] geneSymbols = genes.split(",");
 
         List<Integer> geneRgdIds = new ArrayList<>();
         List<String> termSet = new ArrayList<>();
-        List<String> aspects = new ArrayList<>();
-        aspects.add(aspect);
+        HashMap result = new HashMap();
+
         for (int i = 0; i < geneSymbols.length; i++) {
             Gene g = gdao.getGenesBySymbol(geneSymbols[i],speciesTypeKey);
             geneRgdIds.add(g.getRgdId());
         }
-        LinkedHashMap<String, Integer> geneCounts = adao.getGeneCounts(geneRgdIds, termSet, aspects);
 
 
-        ArrayList result = new ArrayList();
-        int refGenes = dao.getReferenceGeneCount(speciesTypeKey);
-        int inputGenes = geneRgdIds.size();
-        BigDecimal numberOfTerms = new BigDecimal(geneCounts.keySet().size());
-        Iterator tit = geneCounts.keySet().iterator();
-        while (tit.hasNext() ) {
-            HashMap data = new HashMap();
-            String acc = (String) tit.next();
-            String term = oDao.getTermByAccId(acc).getTerm();
-            data.put("acc",acc);
-            data.put("term",term);
-            int refs = geneCounts.get(acc);
-            data.put("count",refs);
-            BigDecimal pvalue =  process.calculatePValue(inputGenes, refGenes, acc, refs, speciesTypeKey);
-            data.put("pvalue", pvalue);
-            BigDecimal bonferroni = process.calculateBonferroni(pvalue,numberOfTerms);
-            data.put("correctedpvalue",bonferroni);
-            result.add(data);
+        ArrayList<String> aspects = new ArrayList<>();
+        aspects.add(Aspect.DISEASE);
+        aspects.add(Aspect.BIOLOGICAL_PROCESS);
+        aspects.add(Aspect.CELLULAR_COMPONENT);
+        aspects.add(Aspect.MOLECULAR_FUNCTION);
+        aspects.add(Aspect.PATHWAY);
+        aspects.add(Aspect.MAMMALIAN_PHENOTYPE);
+        aspects.add(Aspect.CHEBI);
+
+
+        for(int i=0;i<aspects.size();i++) {
+            List<String> aspect = new ArrayList<>();
+            aspect.add(aspects.get(i));
+            LinkedHashMap<String, Integer> geneCounts = adao.getGeneCounts(geneRgdIds, termSet, aspect);
+            int refGenes = dao.getReferenceGeneCount(speciesTypeKey);
+            int inputGenes = geneRgdIds.size();
+            BigDecimal numberOfTerms = new BigDecimal(geneCounts.keySet().size());
+            Iterator tit = geneCounts.keySet().iterator();
+            while (tit.hasNext()) {
+                HashMap data = new HashMap();
+                String acc = (String) tit.next();
+                String term = oDao.getTermByAccId(acc).getTerm();
+                data.put("acc", acc);
+                data.put("term", term);
+                int refs = geneCounts.get(acc);
+                data.put("count", refs);
+                BigDecimal pvalue = process.calculatePValue(inputGenes, refGenes, acc, refs, speciesTypeKey);
+                data.put("pvalue", pvalue);
+                BigDecimal bonferroni = process.calculateBonferroni(pvalue, numberOfTerms);
+                data.put("correctedpvalue", bonferroni);
+                result.put(Aspect.getFriendlyName(aspects.get(i)), data);
+            }
+
         }
-
-
 
         return result;
     }
