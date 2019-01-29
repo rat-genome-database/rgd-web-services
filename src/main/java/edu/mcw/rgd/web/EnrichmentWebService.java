f6cc8e2c67455b874d91d50dd9e1682e20fb6670
harika91 <harika91@users.noreply.github.com>
2019-01-29 08:56:16 -0600

    Update EnrichmentWebService.java

--- src/main/java/edu/mcw/rgd/web/EnrichmentWebService.java
+++ src/main/java/edu/mcw/rgd/web/EnrichmentWebService.java
@@ -82,7 +82,7 @@ class SortbyPvalue implements Comparator<ConcurrentHashMap>
 
     public int compare(ConcurrentHashMap s1, ConcurrentHashMap s2) {
         if(( (BigDecimal)s1.get("pvalue")).compareTo((BigDecimal)s2.get("pvalue")) == 0)
-            return ((BigDecimal)s1.get("count")).compareTo((BigDecimal)s2.get("count"));
+            return Integer.compare(((int)s1.get("count")),((int)s2.get("count")));
         else
             return ( (BigDecimal)s1.get("pvalue")).compareTo((BigDecimal)s2.get("pvalue"));
     }
