41ec3a3fe18bc5fd3438c2200a2174cc7130424f
harika91 <harika91@users.noreply.github.com>
2019-02-01 08:18:27 -0600

    Create EnrichmentGeneRequest.java

---
+++ src/main/java/edu/mcw/rgd/domain/EnrichmentGeneRequest.java
@@ -0,0 +1,13 @@
+package edu.mcw.rgd.domain;
+
+import java.util.List;
+
+/**
+ * Created by hsnalabolu on 1/31/2019.
+ */
+public class EnrichmentGeneRequest {
+
+    public String accId;
+    public int speciesTypeKey;
+    public List<String> geneSymbols;
+}
