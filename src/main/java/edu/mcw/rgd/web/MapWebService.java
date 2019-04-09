50a12a1c13c9e7a290f7c749983aeeccb633336c
harika91 <harika91@users.noreply.github.com>
2019-04-09 11:38:39 -0500

    Create MapWebService.java

---
+++ src/main/java/edu/mcw/rgd/web/MapWebService.java
@@ -0,0 +1,31 @@
+package edu.mcw.rgd.web;
+
+import edu.mcw.rgd.dao.impl.MapDAO;
+import edu.mcw.rgd.datamodel.Map;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by hsnalabolu on 4/9/2019.
+ */
+@RestController
+@Api(tags="Map")
+@RequestMapping(value = "/maps")
+public class MapWebService {
+
+    @RequestMapping(value="/maps", method= RequestMethod.GET)
+    @ApiOperation(value="Return a list of assemblies", tags="Map")
+    public List<Map> getMapsBySpecies(
+            @ApiParam(value="species Key", required=true) @PathVariable(value = "speciesTypeKey") int speciesTypeKey
+
+    ) throws Exception{
+
+        MapDAO mdao= new MapDAO();
+        List<Map> maps = mdao.getMaps(speciesTypeKey);
+        return maps;
+    }
+}
