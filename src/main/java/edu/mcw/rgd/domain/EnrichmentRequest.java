73582c67e7459cc988e2560017d0569f2a7fde02
harika91 <harika91@users.noreply.github.com>
2019-04-16 11:59:45 -0500

    Gene Enrichment services

---
+++ src/main/java/edu/mcw/rgd/domain/EnrichmentRequest.java
@@ -0,0 +1,12 @@
+package edu.mcw.rgd.domain;
+
+import java.util.List;
+
+/**
+ * Created by hsnalabolu on 1/22/2019.
+ */
+public class EnrichmentRequest {
+    public String species;
+    public List<String> genes;
+    public String aspect;
+}
