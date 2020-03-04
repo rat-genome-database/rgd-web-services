89ff5d438d67373ff9dfddc51171cf011bca7bd9
Nalabolu <hsnalabolu@mcw.edu>
2020-03-04 07:44:11 -0600

    Update MapWebService.java

--- src/main/java/edu/mcw/rgd/web/MapWebService.java
+++ src/main/java/edu/mcw/rgd/web/MapWebService.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.TreeMap;
 import java.util.List;
 import java.util.Set;
@@ -20,15 +21,16 @@ import java.util.Set;
 @RequestMapping(value = "/maps")
 public class MapWebService {
 
-    @RequestMapping(value="/{speciesTypeKey}", method= RequestMethod.GET)
+    @RequestMapping(value="/{speciesTypeKey}/{source}", method= RequestMethod.GET)
     @ApiOperation(value="Return a list of assemblies", tags="Map")
     public List<Map> getMapsBySpecies(
-            @ApiParam(value="species Key", required=true) @PathVariable(value = "speciesTypeKey") int speciesTypeKey
+            @ApiParam(value="species Key", required=true) @PathVariable(value = "speciesTypeKey") int speciesTypeKey,
+            @ApiParam(value="source", required=true) @PathVariable(value = "source") String source
 
     ) throws Exception{
 
         MapDAO mdao= new MapDAO();
-        List<Map> maps = mdao.getMaps(speciesTypeKey,"bp");
+        List<Map> maps = mdao.getMaps(speciesTypeKey,"bp",source);
         return maps;
     }
 
