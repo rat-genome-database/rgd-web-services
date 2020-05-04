f5a74f773734afc0e4d2d9b1169f49ec36c813dd
harika91 <harika91@users.noreply.github.com>
2020-05-04 09:59:58 -0500

    Update GeoWebService.java

--- src/main/java/edu/mcw/rgd/web/GeoWebService.java
+++ src/main/java/edu/mcw/rgd/web/GeoWebService.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -27,6 +29,11 @@ public class GeoWebService {
     @ApiOperation(value = "Returns a list experiments in geo for species", tags = "Geo")
     public List<Study> getGeoStudies(@ApiParam(value = "Species", required = true) @PathVariable(value = "species") String species) throws Exception {
 
-        return pdao.getGeoStudies(species);
+        List<Study> result = new ArrayList<Study>();
+        HashMap<String,Study> r = pdao.getGeoStudies(species);
+        for(String gse:r.keySet()){
+            result.add(r.get(gse));
+        }
+        return result;
     }
 }
