5551a900e5e8c06fdb61e1a58277613ede903911
tutajm <mtutaj@mcw.edu>
2021-11-08 17:26:52 -0600

    added SyntenyWebService

---
+++ src/main/java/edu/mcw/rgd/web/SyntenyWebService.java
@@ -0,0 +1,42 @@
+package edu.mcw.rgd.web;
+
+import edu.mcw.rgd.dao.impl.SyntenyDAO;
+import edu.mcw.rgd.datamodel.SyntenicRegion;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by mtutaj on 11/08/2021
+ */
+
+@RestController
+@Api(tags="Synteny")
+@RequestMapping(value = "/synteny")
+public class SyntenyWebService {
+
+    SyntenyDAO sdao = new SyntenyDAO();
+
+    @RequestMapping(value="/blocks/{backboneMapKey}/{backboneChr}/{backboneStart}/{backboneStop}/{mapKey}/{chainLevel}", method= RequestMethod.GET)
+    @ApiOperation(value="Return all synteny blocks for given backbone region", tags = "Synteny")
+    public List<SyntenicRegion> getBlocks(@ApiParam(value="Backbone Species Map Key (available through lookup service)", required=true) @PathVariable(value = "backboneMapKey") String backboneMapKey,
+                                          @ApiParam(value="Backbone Chromosome", required=true) @PathVariable(value = "backboneChr") String chr,
+                                          @ApiParam(value="Backbone Start Position", required=true) @PathVariable(value = "backboneStart") int start,
+                                          @ApiParam(value="Backbone Stop Position", required=true) @PathVariable(value = "backboneStop") int stop,
+                                          @ApiParam(value="Map Key for Comparative Species (available through lookup service)", required=true) @PathVariable(value = "mapKey") int mapKey,
+                                          @ApiParam(value="Chain Level (1, 2, ...)", required=false) @PathVariable(value = "chainLevel") Integer chainLevel ) throws Exception {
+
+        if( chainLevel==null ) {
+            return sdao.getBlocks(backboneMapKey, backboneChr, backboneStart, backboneStop, mapKey);
+        } else {
+            return sdao.getBlocks(backboneMapKey, backboneChr, backboneStart, backboneStop, mapKey, chainLevel);
+        }
+    }
+
+}
