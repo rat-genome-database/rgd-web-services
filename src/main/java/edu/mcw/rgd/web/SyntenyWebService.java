d9d4ba24c4f41308f5fc1f472cf57903970017c2
tutajm <mtutaj@mcw.edu>
2021-11-11 13:10:19 -0600

    synteny: added return of gaps

--- src/main/java/edu/mcw/rgd/web/SyntenyWebService.java
+++ src/main/java/edu/mcw/rgd/web/SyntenyWebService.java
@@ -41,8 +41,45 @@ public class SyntenyWebService {
                                           @ApiParam(value="Backbone Start Position", required=true) @PathVariable(value = "backboneStart") int backboneStart,
                                           @ApiParam(value="Backbone Stop Position", required=true) @PathVariable(value = "backboneStop") int backboneStop,
                                           @ApiParam(value="Map Key for Comparative Species (available through lookup service)", required=true) @PathVariable(value = "mapKey") int mapKey,
-                                          @ApiParam(value="Chain Level (1, 2, ...)", required=true) @PathVariable(value = "chainLevel") Integer chainLevel ) throws Exception {
+                                          @ApiParam(value="Chain Level (1, 2, etc or range: 1-2)", required=true) @PathVariable(value = "chainLevel") String chainLevel ) throws Exception {
 
-        return sdao.getBlocks(backboneMapKey, backboneChr, backboneStart, backboneStop, mapKey, chainLevel);
+        int dashPos = chainLevel.indexOf('-');
+        if( dashPos<0 ) {
+            int level = Integer.parseInt(chainLevel);
+            return sdao.getBlocks(backboneMapKey, backboneChr, backboneStart, backboneStop, mapKey, level);
+        }
+        int minLevel = Integer.parseInt(chainLevel.substring(0, dashPos));
+        int maxLevel = Integer.parseInt(chainLevel.substring(dashPos+1));
+        return sdao.getBlocks(backboneMapKey, backboneChr, backboneStart, backboneStop, mapKey, minLevel, maxLevel);
+    }
+
+    @RequestMapping(value="/gaps/{backboneMapKey}/{backboneChr}/{backboneStart}/{backboneStop}/{mapKey}", method= RequestMethod.GET)
+    @ApiOperation(value="Return gaps for given backbone region", tags = "Synteny")
+    public List<SyntenicRegion> getBlocks(@ApiParam(value="Backbone Species Map Key (available through lookup service)", required=true) @PathVariable(value = "backboneMapKey") int backboneMapKey,
+                                          @ApiParam(value="Backbone Chromosome", required=true) @PathVariable(value = "backboneChr") String backboneChr,
+                                          @ApiParam(value="Backbone Start Position", required=true) @PathVariable(value = "backboneStart") int backboneStart,
+                                          @ApiParam(value="Backbone Stop Position", required=true) @PathVariable(value = "backboneStop") int backboneStop,
+                                          @ApiParam(value="Map Key for Comparative Species (available through lookup service)", required=true) @PathVariable(value = "mapKey") int mapKey ) throws Exception {
+
+        return sdao.getGaps(backboneMapKey, backboneChr, backboneStart, backboneStop, mapKey);
+    }
+
+    @RequestMapping(value="/gaps/{backboneMapKey}/{backboneChr}/{backboneStart}/{backboneStop}/{mapKey}/{chainLevel}", method= RequestMethod.GET)
+    @ApiOperation(value="Return gaps for given backbone region", tags = "Synteny")
+    public List<SyntenicRegion> getBlocks(@ApiParam(value="Backbone Species Map Key (available through lookup service)", required=true) @PathVariable(value = "backboneMapKey") int backboneMapKey,
+                                          @ApiParam(value="Backbone Chromosome", required=true) @PathVariable(value = "backboneChr") String backboneChr,
+                                          @ApiParam(value="Backbone Start Position", required=true) @PathVariable(value = "backboneStart") int backboneStart,
+                                          @ApiParam(value="Backbone Stop Position", required=true) @PathVariable(value = "backboneStop") int backboneStop,
+                                          @ApiParam(value="Map Key for Comparative Species (available through lookup service)", required=true) @PathVariable(value = "mapKey") int mapKey,
+                                          @ApiParam(value="Chain Level (1, 2, etc, or range: '1-2')", required=true) @PathVariable(value = "chainLevel") String chainLevel ) throws Exception {
+
+        int dashPos = chainLevel.indexOf('-');
+        if( dashPos<0 ) {
+            int level = Integer.parseInt(chainLevel);
+            return sdao.getGaps(backboneMapKey, backboneChr, backboneStart, backboneStop, mapKey, level);
+        }
+        int minLevel = Integer.parseInt(chainLevel.substring(0, dashPos));
+        int maxLevel = Integer.parseInt(chainLevel.substring(dashPos+1));
+        return sdao.getGaps(backboneMapKey, backboneChr, backboneStart, backboneStop, mapKey, minLevel, maxLevel);
     }
 }
