6d1293762be0aa00882f0e09699c1c7a989879c5
tutajm <mtutaj@mcw.edu>
2023-12-01 11:38:34 -0600

    vcmap: added method to query comparable species

---
+++ src/main/java/edu/mcw/rgd/domain/vcmap/ComparableSpecies.java
@@ -0,0 +1,9 @@
+package edu.mcw.rgd.domain.vcmap;
+
+public class ComparableSpecies {
+
+    public int speciesTypeKey;
+    public String speciesCommonName;
+    public int mapKey;
+    public String mapName;
+}
